87024f6b0d76bbe3b5e3f0713ef2a9c4afbd414d
Ayush Sharma <ayushhardeniya.grid@gmail.com>
2025-08-01 18:43:57 +0530

    Changing repo name, hence updating vite.config.ts & package.json 'homepage'

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/website/", // ✅ required for user GitHub Pages repo
+  base: "/", // ✅ required for user GitHub Pages repo
   plugins: [react()],
   resolve: {
     alias: {
