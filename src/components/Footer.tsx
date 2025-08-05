dfe2020040fadcd455644e4160aa44d220efb9a7
Ayush Sharma <ayushhardeniya.grid@gmail.com>
2025-08-05 11:15:26 +0530

    Fixed logo for remote url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -48,7 +48,7 @@ const Footer = () => {
             <div className="flex items-center space-x-3 mb-6">
               <div className="w-11 h-11 rounded-full overflow-hidden">
                 <img
-                  src="public\assets\zenyukti-logo.png"
+                  src="/assets/zenyukti-logo.png"
                   alt="zenyukti_logo"
                   className="w-full h-full object-cover"
                 />
